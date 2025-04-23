ae3a6cefd19f9e827ab75ea440a13601087ae783
hi-lufz1 <latifufauzi@gmail.com>
2025-04-23 20:01:16 +0700

    menambahkann  text field email

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -10,6 +10,24 @@ class LoginPage extends StatefulWidget {
 class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Form(child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [TextFormField(
+                decoration: const InputDecoration(labelText: 'Email'),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter your email';
+                  }
+                  return null;
+                },
+              ),
+          ],
+        ),
+        )
+        ),
+    );
   }
 }
