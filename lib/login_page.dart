56ed65654a3c1b9e58d90eac6aa37190d50b01a8
hi-lufz1 <latifufauzi@gmail.com>
2025-04-23 20:07:43 +0700

    menambahkan controller dan form key

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -8,15 +8,22 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+
+
   @override
   Widget build(BuildContext context) {
+    final _formKey = GlobalKey<FormState>();
     return Scaffold(
-      body: Form(child: Padding(
+      body: Form(
+        key: _formKey,child: Padding(
         padding: const EdgeInsets.all(16.0),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             TextFormField(
+                controller: emailController,
                 decoration: const InputDecoration(labelText: 'Email'),
                 validator: (value) {
                   if (value == null || value.isEmpty) {
@@ -26,6 +33,7 @@ class _LoginPageState extends State<LoginPage> {
                 },
               ),
             TextFormField(
+                controller: passwordController,
                 decoration: const InputDecoration(labelText: 'Password'),
                 obscureText: true,
                 validator: (value) {
