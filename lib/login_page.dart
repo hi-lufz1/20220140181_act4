e6af9372c8a0420361b85c01b6ad3b28271a7138
hi-lufz1 <latifufauzi@gmail.com>
2025-04-23 20:20:14 +0700

    menambahkann inisiasi state

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -11,6 +11,10 @@ class _LoginPageState extends State<LoginPage> {
   final TextEditingController emailController = TextEditingController();
   final TextEditingController passwordController = TextEditingController();
 
+  @override
+  void initState() {
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
